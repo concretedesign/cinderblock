f400d9ff067c9a6d363eceb454b09f69cff701e3
Matt Crider <mattcrider@gmail.com>
2015-05-25 14:10:20 -0400

    Format work times

--- client/components/timeline/timeline.js
+++ client/components/timeline/timeline.js
@@ -1,6 +1,19 @@
 Template.timeline.helpers({
   employee_clients: function () {
-    var id = this._id;
-    return EmployeeClients.find({ employee_id: id.valueOf() });
+    var id = this._id.valueOf();
+    return EmployeeClients.find({ employee_id: id });
+  },
+  clientName: function (clientId) {
+    return Clients.findOne(clientId).name;
+  },
+  formatWork: function (workId) {
+    var work = Work.findOne(new Meteor.Collection.ObjectID(workId));
+    if (work) {
+      var start = new Date(work.start);
+      var end = new Date(work.end);
+      return 'L' + work.hotness + ': ' + start.toLocaleDateString() + ' - ' + end.toLocaleDateString();
+    } else {
+      return '';
+    }
   }
 });
