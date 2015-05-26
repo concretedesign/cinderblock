61b5daab65b4b1e1c0cbb76e2ce01c27123ccbcd
Matt Crider <mattcrider@gmail.com>
2015-05-26 15:28:36 -0400

    Draw work timelines as bars

--- client/components/timeline/timeline.js
+++ client/components/timeline/timeline.js
@@ -1,19 +1,43 @@
+const dayWidth = 20;
+
 Template.timeline.helpers({
   employee_clients: function () {
     var id = this._id.valueOf();
     return EmployeeClients.find({ employee_id: id });
   },
   clientName: function (clientId) {
-    return Clients.findOne(clientId).name;
+    return Clients.findOne(clientId).name || '';
+  },
+  clientColor: function (clientId) {
+    return Clients.findOne(clientId).color || '#000000';
   },
   formatWork: function (workId) {
     var work = Work.findOne(new Meteor.Collection.ObjectID(workId));
     if (work) {
-      var start = new Date(work.start);
-      var end = new Date(work.end);
-      return 'L' + work.hotness + ': ' + start.toLocaleDateString() + ' - ' + end.toLocaleDateString();
+      var start = moment(work.start);
+      var end = moment(work.end);
+
+      // For x position, get difference between minDate and start
+      var daysFromStart = moment(Session.get('minDate')).diff(start, 'days');
+      var posX = daysFromStart * dayWidth;
+
+      // For width, get difference between start and end
+      var jobDays = moment(end).diff(start, 'days');
+      var width = jobDays * dayWidth;
+
+      // For height, translate hotness
+      var height = (work.hotness + 1) * 4;
+
+      return Spacebars.SafeString(
+        '<div class="work-bar" style="left: ' + posX +
+          'px; width: ' + width + 'px; height: ' + height + 'px;"></div>'
+      );
     } else {
       return '';
     }
+  },
+  barWidth: function () {
+    var dateDiff = Session.get('dateDiff') || 365;
+    return (dateDiff * dayWidth) + 'px';
   }
 });
