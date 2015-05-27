f09080c526839ca55cdd8b009e89291c309e5441
Matt Crider <mattcrider@gmail.com>
2015-05-27 16:08:13 -0400

    Client add saves to DB

--- client/components/employee/employee.js
+++ client/components/employee/employee.js
@@ -1,42 +1,30 @@
 
 Template.employee.onRendered(function () {
   interact('.profile').dropzone({
-    // only accept elements matching this CSS selector
     accept: '.client',
-    // Require a 75% element overlap for a drop to be possible
-    // overlap: 0.75,
 
-    // listen for drop related events:
-
-    ondropactivate: function (event) {
-      // add active dropzone feedback
-      console.log('ondropactivate');
-      event.target.classList.add('drop-active');
-    },
     ondragenter: function (event) {
-      var draggableElement = event.relatedTarget,
-          dropzoneElement = event.target;
-
-      console.log('ondrageenter');
+      var draggableElement = event.relatedTarget;
+      var dropzoneElement = event.target;
 
       // feedback the possibility of a drop
       dropzoneElement.classList.add('drop-target');
       draggableElement.classList.add('can-drop');
     },
     ondragleave: function (event) {
-      console.log('ondragleave');
-      // remove the drop feedback style
       event.target.classList.remove('drop-target');
       event.relatedTarget.classList.remove('can-drop');
     },
     ondrop: function (event) {
-      console.log('ondrop');
-    },
-    ondropdeactivate: function (event) {
-      console.log('ondropdeactivate');
-      // remove active dropzone feedback
-      event.target.classList.remove('drop-active');
       event.target.classList.remove('drop-target');
+      event.relatedTarget.classList.remove('can-drop');
+      
+      var clientId = event.relatedTarget.dataset.client_id;
+      var employeeId = event.target.dataset.employee_id;
+
+      EmployeeClients.insert(
+        { client_id: clientId, employee_id: employeeId, work: [] }
+      );
     }
   });
 });
