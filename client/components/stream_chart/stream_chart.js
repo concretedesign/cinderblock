247522c8080c6916315c9f1964d38ce5b4a55e29
Matt Crider <mattcrider@gmail.com>
2015-06-08 10:13:52 -0400

    Chart shows actual data + client colors

--- client/components/stream_chart/stream_chart.js
+++ client/components/stream_chart/stream_chart.js
@@ -22,90 +22,151 @@ Template.stream_chart.events({
   },
   'click .stream-employee': function (e) {
     Session.set('streamChartEmployeeId', this._id)
-    streamViz.transition();
+    streamViz.transition(this._id);
   }
 });
 
 Template.stream_chart.onRendered(function () {
   window.streamViz = new Viz();
+  // setTimestreamViz.transition(Employees.findOne()._id);
 })
 
 function Viz () {
-  var n, m, stack, layers0, layers1, width, height, x, y, color, area, svg;
+  var startDate, endDate, stack, nest, layers, width, height, x, y, color, area, svg, format, numDays, colors;
 
+
+
+  var _getClientColors = function () {
+    var clientColors = [];
+    Clients.find().forEach(function (client) {
+      clientColors[client._id] = client.color;
+    })
+    return clientColors;
+  }
+
+  // startDate and endDate should be moment objects
+  var _getEmployeeData = function(employeeId, startDate, endDate) {
+    var data = [];
+    EmployeeClients.find({ employee_id: employeeId}).forEach(function (employeeClient) {
+      var client = Clients.findOne(employeeClient.client_id);
+
+      var clientDates = [];
+      // Fill baseline dates
+      for (var j = 0; j < numDays; j++) {
+        var date = moment(startDate).add(j, 'days');
+        clientDates.push({
+          clientId: client._id,
+          hotness: 1,
+          date: date.format('MM/DD/YY')
+        })
+      }
+
+      employeeClient.work.forEach(function (workId) {
+        var work = Work.findOne(workId);
+
+        // split work into day units
+        var days = Math.abs(moment(work.start).diff(moment(work.end), 'days'));
+        for (var i = 0; i < days; i++) {
+          var date = moment(work.start).add(i, 'days');
+          if (date.isBetween(startDate.startOf('day'), endDate.endOf('day'))) {
+
+            var index = _.indexOf(_.pluck(clientDates, 'date'), date.format('MM/DD/YY'));
+            clientDates[index].hotness = work.hotness + 1; // Add one because we've set 0 to 1 above
+          }
+        }
+      });
+      data = data.concat(clientDates);
+    });
+
+    return data;
+  }
+
+  var _setDates = function(x) {
+    // TODO
+    // x.domain([startDate.toDate(), endDate.toDate()]);
+  }
+
+
+  var _transition = function (employeeId) {
+    var data = _getEmployeeData(employeeId, startDate, endDate);
+    data.forEach(function(d) {
+      d.date = format.parse(d.date);
+      d.hotness = +d.hotness;
+    });
+
+    layers = stack(nest.entries(data));
+
+    var paths = svg.selectAll("path").data(layers)
+
+    paths.enter()
+      .append("path")
+      .attr("d", function (d) { return area(d.values); })
+      .style("fill", function (d) {
+        return colors[d.key]; // FIXME: Should be looked up in an array of employeeId => employeeColors
+      })
+
+    svg.selectAll("path").attr("d", function (d) { return area(d.values); })
+      .style("fill", function (d) {
+        return colors[d.key]; // FIXME: Should be looked up in an array of employeeId => employeeColors
+      }).transition().duration(1500);
+
+
+    paths.exit().remove();
+  }
 
   var _init = function () {
-    n = 20; // number of layers
-    m = 200; // number of samples per layer
-    stack = d3.layout.stack().offset("wiggle");
-    layers0 = stack(d3.range(n).map(function() { return _bumpLayer(m); }));
-    layers1 = stack(d3.range(n).map(function() { return _bumpLayer(m); }));
+    numDays = 26;
+    startDate = moment().startOf('isoweek');
+    endDate = moment().startOf('isoweek').add(numDays, 'days');
+
+    colors = _getClientColors();
+
+    stack = d3.layout.stack().offset("zeros")
+      .values(function (d) { return d.values; })
+      .x(function (d) { return d.date; })
+      .y(function (d) { return d.hotness; });
+
+    nest = d3.nest()
+      .key(function (d) { return d.clientId; });
+
+    area = d3.svg.area()
+      .interpolate("cardinal")
+      .x(function(d) { return x(d.date); })
+      .y0(function(d) { return y(d.y0); })
+      .y1(function(d) { return y(d.y0 + d.y); });
 
     width = $('.viz').width();
     height = $('.viz').width() / 2;
 
-    x = d3.scale.linear()
-      .domain([0, m - 1])
+    format = d3.time.format("%m/%d/%y");
+
+    x = d3.time.scale()
+      .domain([startDate.toDate(), endDate.toDate()])
       .range([0, width]);
 
     y = d3.scale.linear()
-      .domain([0, d3.max(layers0.concat(layers1), function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); })])
+      .domain([0, 10])
       .range([height, 0]);
 
     color = d3.scale.linear()
       .range(["#aad", "#556"]);
 
-    area = d3.svg.area()
-      .x(function(d) { return x(d.x); })
-      .y0(function(d) { return y(d.y0); })
-      .y1(function(d) { return y(d.y0 + d.y); });
-
     svg = d3.select(".viz").append("svg")
       .attr("width", width)
       .attr("height", height);
 
-    svg.selectAll("path")
-      .data(layers0)
-    .enter().append("path")
-      .attr("d", area)
-      .style("fill", function() { return color(Math.random()); });
+    // svg.selectAll("path")
+    //   .data(layers0)
+    // .enter().append("path")
+    //   .attr("d", area)
+    //   .style("fill", function() { return color(Math.random()); });
   }
 
-  var _bumpLayer = function (n) {
-    function bump(a) {
-      var x = 1 / (.1 + Math.random()),
-          y = 2 * Math.random() - .5,
-          z = 10 / (.1 + Math.random());
-      for (var i = 0; i < n; i++) {
-        var w = (i / n - y) * z;
-        a[i] += x * Math.exp(-w * w);
-      }
-    }
-
-    var a = [], i;
-    for (i = 0; i < n; ++i) a[i] = 0;
-    for (i = 0; i < 5; ++i) bump(a);
-    var layer = a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
-    return layer;
-  }
-
-  var _transition = function () {
-    d3.select(".viz").selectAll("path")
-      .data(function() {
-        var d = layers1;
-        layers1 = layers0;
-        return layers0 = d;
-      })
-    .transition()
-      .duration(1500)
-      .attr("d", area);
-  }
 
   // Initialize our variables
   _init();
 
   return {
-    bumpLayer: _bumpLayer,
     transition: _transition
   }
 }
